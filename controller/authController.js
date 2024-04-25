e24571e118ee95ea89257b7dbc0f31bd95f83eb4
Gabriel345 <gabrielmzenerato242@gmail.com>
2024-04-25 08:28:53 -0300

    alteração autenticação em authcontroller de logar
    com username para email

--- controller/authController.js
+++ controller/authController.js
@@ -3,24 +3,21 @@ const bcrypt = require('bcrypt');
 const createError = require('http-errors');
 
 exports.login = async (req, res) => {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
 
     try {
-        const user = await User.findOne({ username });
+        const user = await User.findOne({ email });
 
-        // Verificar se o usuário existe
         if (!user) {
             return res.status(401).json({ message: 'Credenciais inválidas' });
         }
 
-        // Comparar a senha fornecida com o hash da senha armazenada no banco de dados
         const passwordMatch = await bcrypt.compare(password, user.passwordHash);
 
         if (!passwordMatch) {
             return res.status(401).json({ message: 'Credenciais inválidas' });
         }
 
-        // Se a senha corresponder, definir o ID do usuário na sessão
         req.session.userId = user._id;
         res.redirect('/');
     } catch (error) {
