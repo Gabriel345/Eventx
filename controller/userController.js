a40b4e9f64d632c5510682faa98a4be426425e59
Gabriel345 <gabrielmzenerato242@gmail.com>
2024-05-29 09:36:49 -0300

    modificação front

--- controller/userController.js
+++ controller/userController.js
@@ -36,32 +36,37 @@ exports.getAllUsers = async (req, res) => {
 
 exports.getUserProfile = async (req, res) => {
   try {
-      // Verificar se o usuário está autenticado
-      if (!req.session.userId) {
-          return res.redirect('/login');
-      }
+    // Verificar se o usuário está autenticado
+    const userId = req.session.userId; // Obtenha o userId da sessão
+    if (!userId) {
+      return res.redirect('/login');
+    }
 
-      // Encontrar o usuário pelo ID
-      const user = await User.findById(req.session.userId);
+    // Encontrar o usuário pelo ID
+    const user = await User.findById(userId);
 
-      // Verificar se o usuário existe
-      if (!user) {
-          return res.status(404).json({ message: 'Usuário não encontrado' });
-      }
+    // Verificar se o usuário existe
+    if (!user) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
 
-      // Encontrar os eventos criados pelo usuário
-      const userEvents = await Event.find({ organizer: req.session.userId });
+    // Encontrar os eventos criados pelo usuário
+    const userEvents = await Event.find({ organizer: userId });
 
-      // Encontrar os eventos cadastrados pelo usuário
-      const registeredEvents = await Event.find({ 'participants.user': req.session.userId });
+    // Encontrar os eventos cadastrados pelo usuário
+    const registeredEvents = await Event.find({ 'participants.user': userId });
 
-      // Renderizar a página de perfil com os eventos criados e cadastrados pelo usuário
-      res.render('profile', { user, userEvents, registeredEvents });
+    // Passar os eventos criados e cadastrados pelo usuário para o template de página
+    res.locals.userId = userId; // Passa o userId para a visualização
+    res.locals.userEvents = userEvents;
+    res.locals.registeredEvents = registeredEvents;
+
+    // Renderizar a página de perfil
+    res.render('profile', { user });
   } catch (error) {
-      res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
-
 exports.updateUser = async (req, res) => {
   try {
     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
